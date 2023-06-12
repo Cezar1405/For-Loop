17f87f8b670add14b78c73898db9ac685980757e
Cezar1405 <cicg.14.05@gmail.com>
2023-06-12 18:08:18 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //
+        //Use un ciclo for para imprimir todos los números del 5 al 25
+        // (incluidos los límites superior e inferior)
+        for(int i = 5; i < 26 ; i++){
+            System.out.println(i);
+        }
+    }
+}
